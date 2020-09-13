86879cf2273f304f2a449adbfbcb06c48afb946a
Anumeha Pandey <anumeha.pandey@globallogic.com>
2020-09-13 23:39:40 +0530

    Domain.core changes

--- MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -11,7 +11,6 @@ namespace MicroRabbit.Domain.Core.Bus
    public interface IEventBus
     {
         Task SendCommand<T>(T command) where T : Command;
-
         void Publish<T>(T @event) where T : Event;
         void Subscribe<T,TH>()
             where T : Event
